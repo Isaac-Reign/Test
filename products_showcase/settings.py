428dbb1681bb9430dba8eee6d1a562bf9ea194f6
Isaac Reign <122174090+Isaac-Reign@users.noreply.github.com>
2024-02-24 08:18:21 +0000

    Update settings.py

--- products_showcase/settings.py
+++ products_showcase/settings.py
@@ -21,8 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#%k8k*3-63@h*2=nf(za-o&dio53k)3zxv3&_o2h)w8r%&v@v#'
+#'django-insecure-#%k8k*3-63@h*2=nf(za-o&dio53k)3zxv3&_o2h)w8r%&v@v#'
 
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -51,7 +53,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
