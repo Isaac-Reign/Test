b086b7cf04e89565184fc716e25fb2c3e561beae
Isaac Reign <122174090+Isaac-Reign@users.noreply.github.com>
2024-02-24 07:16:46 +0000

    Update settings.py

--- products_showcase/settings.py
+++ products_showcase/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
 
     'django.contrib.sitemaps',
-    'store.apps.StoreConfig',
+    'store',
 
     # 'django_otp',
     # 'django_otp.plugins.otp_totp',
@@ -129,14 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = "/media/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'store/static'),
 ]
-MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
