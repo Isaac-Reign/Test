bb320ea4b58b69b00352a3f3c2233304dc0f12d9
Project <isaac.developer99@gmail.com>
2024-02-24 17:09:38 +0000

    update settings.py

--- products_showcase/settings.py
+++ products_showcase/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'store/static')
- ]
+#STATICFILES_DIRS = [
+ #    os.path.join(BASE_DIR, 'store/static')
+# ]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
