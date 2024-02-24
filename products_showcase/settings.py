1b0c99d7bd4ec31c6700e15bfa609fefdb4a8d73
Project <isaac.developer99@gmail.com>
2024-02-24 15:00:54 +0000

    update settings.py

--- products_showcase/settings.py
+++ products_showcase/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv('DEBUG'))
-DEBUG = False
+DEBUG = bool(os.getenv('DEBUG'))
 ALLOWED_HOSTS = ["*"]
 
 
