e98cd03f08edd31e8454c7b17f90db3a117ab6ad
Isaac Reign <122174090+Isaac-Reign@users.noreply.github.com>
2024-02-25 08:27:47 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,15 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations
 python manage.py migrate
 
+#set -o errexit
+#pip install -r r.txt
+#python manage.py collectstatic --no-input
+#python manage.py makemigrations
+#python manage.py migrate
 
-python manage.py createsuperuser --no-input
-python manage.py loaddata store/fixtures/products.json	
-python manage.py loaddata store/fixtures/news.json
+
+#python manage.py createsuperuser --no-input
+#python manage.py loaddata store/fixtures/products.json	
+#python manage.py loaddata store/fixtures/news.json
